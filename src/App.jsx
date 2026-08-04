e20a7fd012aa0ce75e9fb94c40a720c5d6816840
amirmalikzade <https://github.com/amiralimalikzade1981/QAutomation-NG.git>
2026-08-04 14:09:03 +0400

    Add task3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,47 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import Authentication from './components/Authentication';
+import Dashboard from './components/Dashboard';
+
+import './App.css';
+
 function App() {
-  return <div className="App"></div>;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userUsername, setUserUsername] = useState('');
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem('accessToken');
+
+    if (!accessToken) {
+      return;
+    }
+
+    axios
+      .post(
+        '/api/auth/',
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        },
+      )
+      .then((response) => {
+        setIsLoggedIn(true);
+        setUserUsername(response.data.username);
+      })
+      .catch(() => {
+        setIsLoggedIn(false);
+        setUserUsername('');
+      });
+  }, []);
+
+  return isLoggedIn ? (
+    <Dashboard userUsername={userUsername} />
+  ) : (
+    <Authentication />
+  );
 }
 
-export default App
+export default App;
